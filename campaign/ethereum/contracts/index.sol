7f54a160cf4fde18526d9949b7c3f6cbcb4c8632
Fernando Narbona <fnarbona@tracfone.com>
2018-02-05 16:49:57 -0500

    testing file for kickstarter

---
+++ campaign/ethereum/contracts/index.sol
@@ -0,0 +1,88 @@
+pragma solidity ^0.4.17;
+
+// KICKSTARTER CREATOR
+contract kickstarterCreator {
+  address[] public kickstarters;
+
+  function createKickstarter(uint minContribution) public {
+    address newKickstarter = new Kickstarter(minContribution, msg.sender);
+    kickstarters.push(newKickstarter);
+  }
+
+  function viewAllKickstarters() public view returns (address[]) {
+    return kickstarters;
+  }
+}
+
+// KICKSTARTER INSTANCE
+contract Kickstarter {
+  struct Request {
+    string reqDescription;
+    uint reqCost;
+    address reqVendor;
+    bool reqCompletion;
+    uint approvalCount;
+    mapping(address => bool) reqApprovals;
+  }
+
+  Request[] public requests;
+  address public projectOwner;
+  uint public minContribution;
+  mapping(address => bool) public approvers;
+  uint public approversCount;
+
+  modifier restricted() {
+    require(msg.sender == projectOwner);
+    _;
+  }
+
+  function Kickstarter(uint minValue, address creator) public {
+    projectOwner = creator;
+    minContribution = minValue;
+  }
+
+  function contribute() public payable {
+    require(msg.value > minContribution);
+
+    if (!approvers[msg.sender]) {
+      approvers[msg.sender] = true;
+      approversCount++;
+    }
+  }
+
+  function createRequest(string description, uint cost, address vendor)
+  public restricted {
+    Request memory newRequest = Request({
+        reqDescription: description,
+        reqCost: cost,
+        reqVendor: vendor,
+        reqCompletion: false,
+        approvalCount: 0
+      });
+
+    requests.push(newRequest);
+  }
+
+  function approveRequest(uint index) public {
+    Request storage myRequest = requests[index];
+
+    require(approvers[msg.sender] == true);
+    require(!myRequest.reqApprovals[msg.sender]);
+
+    myRequest.reqApprovals[msg.sender] = true;
+    myRequest.approvalCount++;
+  }
+
+  function finalizeRequest(uint index)
+  public
+  payable
+  restricted {
+    Request storage myRequest = requests[index];
+
+    require(myRequest.approvalCount > (approversCount/2));
+    require(!myRequest.reqCompletion);
+
+    myRequest.reqVendor.transfer(myRequest.reqCost);
+    myRequest.reqCompletion = true;
+  }
+}
---
+++ campaign/test/index.test.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+// compiled file reference
+const compiledCreator = require('../ethereum/build/kickstarterCreator.json');
+const compiledKickstarter = require('../ethereum/build/Kickstarter.json');
+
+let accounts;
+let creator;
+let kickstarterAddress;
+let kickstarter;
+
+beforeEach(async () => {
+  // retrieve accounts
+  accounts = await web3.eth.getAccounts();
+  // deploy contract
+  creator = await new web3.eth.Contract(JSON.parse(compiledCreator.interface))
+    .setProvider(provider)
+    .deploy({data: compiledCreator.bytecode})
+    .send({from: accounts[0], gas: '1000000'});
+  // create a kickstarter from the deployed contract
+  await creator.methods
+    .createKickstarter('100')
+    .send({from: accounts[0], gas: '1000000'});
+  // view all kickstarters and select the first one
+  const temp = await creator.methods.viewAllKickstarters().call();
+  kickstarterAddress = temp[0]
+  kickstarter = await new web3.eth.Contract(
+    JSON.parse(compiledKickstarter.interface),
+    kickstarterAddress
+  );
+});
+
+describe('kickstarters', () => {
+  it('deploys creator and kickstarter contract', () => {
+    // from beforeEach, verify that the contract was deployed
+    // and that both the creator and the newly created
+    // kickstarter has an address
+    assert.ok(creator.options.address);
+    assert.ok(kickstarter.options.address);
+  });
+});
