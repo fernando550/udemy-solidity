8e16631c6e7e1c3ffaa693e57dbf4a493bb76e78
Fernando Narbona <fnarbona@tracfone.com>
2018-02-07 16:26:08 -0500

    testing file continued...

--- campaign/test/index.test.js
+++ campaign/test/index.test.js
@@ -56,13 +56,39 @@ describe('kickstarters', () => {
 
   it('requires minimum contribution', async () => {
     try {
-      await kickstarter.methods.contribute().send({
-        from: accounts[1],
-        value: '5'
-      });
+      await kickstarter.methods.contribute()
+        .send({from: accounts[1], value: '5'});
+
       assert(false);
     } catch (err) {
       assert(err);
-    }
-  })
+    };
+  });
+
+  it('allows a manager to make a payment request', async () => {
+    await kickstarter.methods.createRequest('Buy batteries', '100', accounts[1])
+      .send({from: accounts[0], gas: '1000000'});
+
+    const request = await kickstarter.methods.requests(0).call();
+    assert.equal('Buy batteries', request.reqDescription);
+  });
+
+  it('processes requests', async () => {
+    await kickstarter.methods.contribute()
+      .send({from: accounts[0], value: web3.utils.toWei('10', 'ether')});
+
+    await kickstarter.methods.createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({from: accounts[0], gas: '1000000'});
+
+    await kickstarter.method.approveRequest(0)
+      .send({from: accounts[0], gas: '1000000'});
+
+    await kickstarter.method.finalizeRequest(0)
+      .send({from: accounts[0], gas: '1000000'});
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+    assert(balance > 104);
+  });
 });
